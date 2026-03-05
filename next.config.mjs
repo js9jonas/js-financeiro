232e846d9351753db77471fa938f960d7e27b539
Jonas Eduardo <js9jonas@gmail.com>
2026-03-04 21:00:05 -0300

    ajuste next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,16 @@
-/** @type {import("next").NextConfig} */const nextConfig = {  experimental: {    serverActions: true,    serverComponentsExternalPackages: ["pg"],  },};export default nextConfig;
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["pg"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [{ key: "Cache-Control", value: "no-store" }],
+      },
+    ];
+  },
+};
+export default nextConfig;
